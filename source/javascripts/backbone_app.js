9b5904d41f49409d45623e5377eaed2443835edb
ichabodcole <ichabodcole@gmail.com>
2013-04-10 13:34:48 -0700

    Author: Cole Commit: Update middleman default css Add static json file to fill notes collection Implement basic backbone functionality

--- source/javascripts/backbone_app.js
+++ source/javascripts/backbone_app.js
@@ -1,10 +1,56 @@
 $(function(){
-  // var appModel = Backbone.Model.extend({
+  var Note = Backbone.Model.extend({
+    urlRoot: '/notes'
+  });
+
+  var NoteList = Backbone.Collection.extend({
+    model: Note,
+    url: "/notes/index.json",
+    initialize: function(){
+
+    }
+  });
+
+  var ListItem = Backbone.View.extend({
+    tagName: "li",
+    initialize: function(options){
+      this.model = options.model;
+      this.render();
+    },
+
+    render: function(){
+      this.$el.html(this.model.get('text'));
+    }
+  });
+
+  var ListView = Backbone.View.extend({
+    el: "#notes",
+    initialize: function(){
+      this.listenTo(Notes, "add", this.addOne);
+      this.listenTo(Notes, 'reset', this.addAll);
+      Notes.fetch();
+    },
+
+    addAll: function(){
+
+    },
+
+    addOne: function(model){
+      var note = new ListItem({model:model});
+      this.$el.append(note.el);
+    },
+
+    render: function(){
+
+    }
+  });
+
+
+  // $('#my-button').onClick(function(e){
 
   // });
 
-  // var appCollection = Backbone.Collection.extend({
-
-  // });
+  var Notes = new NoteList();
+  var appView = new ListView();
 });
 
